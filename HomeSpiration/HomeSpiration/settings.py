c190bcacfcec568ecbcfcb89de8f8de7c01e4219
UthrajaUthaman <119808929+UthrajaUthaman@users.noreply.github.com>
2024-04-18 10:44:41 +0530

    initial commit

--- HomeSpiration/HomeSpiration/settings.py
+++ HomeSpiration/HomeSpiration/settings.py
@@ -58,7 +58,7 @@ SECRET_KEY = 'django-insecure-q*(p7zg4udflc44iktscsd9sv*unqwo31y^v+_2ohnr80!fa1o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 LOGIN_URL = 'login'
 # Application definition
